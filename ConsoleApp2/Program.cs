3ee769558ea6eb3ace5af053656f87e9b670dd5e
E-Svensson <edvin.sven123@gmail.com>
2022-10-19 18:40:21 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Svar = "j";
+
+            Console.Write("Skriv in ett tal: "); int InputTal1 = int.Parse(Console.ReadLine());
+
+            int StörstaTal = InputTal1;
+
+            while (Svar == "j")
+            {
+                Console.Write("\nVill du skriva in ett till tal? (j/n): "); Svar = Console.ReadLine();
+
+                if (Svar == "n")
+                {
+                    break;
+                }
+
+                Console.Write("\nSkriv in ett tal: "); int InputTal2 = int.Parse(Console.ReadLine());
+
+                if (InputTal2 >= InputTal1)
+                {
+                    InputTal2 = StörstaTal;
+                }
+            }
+
+            Console.WriteLine($"Det största talet du skrev ut var {StörstaTal}");
+        }
+    }
+}
